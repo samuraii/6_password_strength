ae96aa6da0ec9737c95283ce62e7d90b5821ff65
mchirkov <mchrikov@ivi.ru>
2018-03-28 16:28:41 +0300

    Создал каркас функций

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,46 @@
+import re
+import getpass
+
+
+def get_password_from_user():
+    user_password = getpass.getpass('Enter password (minimum len: 8):')
+    if len(user_password) < 8:
+        print('Password is too short.')
+        return get_password_from_user()
+    else:
+        return user_password
+
+
+def check_digits(password):
+    pass
+
+
+def check_case_sensitivity(password):
+    pass
+
+
+def check_special_characters(password):
+    pass
+
+
+def check_blacklist(password):
+    pass
+
+
+def check_for_common_templates(password):
+    pass
 
 
 def get_password_strength(password):
+    password_checklist = [
+        check_special_characters,
+        check_case_sensitivity,
+        check_blacklist,
+        check_digits,
+        check_for_common_templates
+    ]
     pass
 
 
 if __name__ == '__main__':
-    pass
+    get_password_from_user()
